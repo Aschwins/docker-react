cea89d9a89bb22e617e328cfc5f20d2371f1b561
Aschwins <aschwin.schilperoort@gmail.com>
2021-04-26 11:27:59 +0200

    added dockercompose and added tests

---
+++ my-app/src/__tests__/index.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+test('loads and displays greeting', async () => {
+    // Arrange
+    // Act
+    // Assert
+});
+
+test('loads and displays greeting', async () => {
+    // Arrange
+    // Act
+    // Assert
+});
